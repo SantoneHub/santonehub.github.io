7afc7aa49528cfc82279db2b7ac827dcdc831438
SantoneHub <125828707+SantoneHub@users.noreply.github.com>
2023-02-27 14:01:37 -0300

    Initial Commit

---
+++ js/function.js
@@ -0,0 +1,15 @@
+
+
+
+
+document.getElementById('telefono').addEventListener('click', function () {
+    document.getElementById('numero').innerHTML = "+54 9 1234 112233"
+});
+
+document.getElementById('email').addEventListener('click', function () {
+    document.getElementById('correo').innerHTML = "louis.ellington@email.com"
+});
+
+document.getElementById('pais').addEventListener('click', function () {
+    document.getElementById('lugar').innerHTML = "Argentina"
+});
